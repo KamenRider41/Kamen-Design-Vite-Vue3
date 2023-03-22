d03f80210a7609dfaad0b33e7a2e94946e779c2f
kamenrider41 <229335832@qq.com>
2023-03-22 21:13:26 +0800

    fix: 指定打包css文件格式，取消自动hash

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: 41
  * @Date: 2023-03-15 09:42:26
  * @LastEditors: 41
- * @LastEditTime: 2023-03-22 17:54:05
+ * @LastEditTime: 2023-03-22 21:01:32
  * @Description: 
  */
 /// <reference types="vitest" />
@@ -13,10 +13,12 @@ import Unocss from "./config/unocss";
 // https://vitejs.dev/config/
 const rollupOptions = {
   external: ["vue", "vue-router"],
-  output: {
+  output:
+  {
     globals: {
       vue: "Vue",
     },
+    assetFileNames: `[name].[ext]`,
   },
 };
 export default defineConfig({
@@ -29,6 +31,7 @@ export default defineConfig({
   ],
   // 添加库模式配置
   build: {
+    // @ts-ignore
     rollupOptions,
     minify: false,
     cssCodeSplit: true,   // 追加
@@ -37,7 +40,7 @@ export default defineConfig({
       name: "KamenDesign",
       fileName: "Kamen-design",
       // 导出模块格式
-      formats: ["es", "umd", "iife"],
+      formats: ["es", "umd", "iife"],/*  */
     },
   },
   test: {
@@ -49,6 +52,17 @@ export default defineConfig({
     // 支持tsx组件，很关键
     transformMode: {
       web: [/.[tj]sx$/]
+    }
+  },
+  css: {
+    modules: {
+      generateScopedName: (name, filename, css) => {
+        // name -> 代表的是你此刻css文件中的类名
+        // filename -> 是你当前css文件的绝对路径
+        // css -> 给的就是你当前样式
+        // 配置成函数以后, 返回值就决定了他最终显示的类型
+        return `${name}_${Math.random().toString(36).substr(3, 8)}`;
+      }
     }
   }
 
